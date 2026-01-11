139934cad8314e8049f3e2aaa317b3dda57b3c2b
lizi dekanozishvili <lizidekanozishvili14@gmail.com>
2026-01-11 18:22:34 +0000

    Finish custom User model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from blog.models import Tag, Post, Comment, AuthorProfile
 # Register your models here.
+admin.site.register(AuthorProfile)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class AuthorProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.__class__.__name__} object for {self.user}"
